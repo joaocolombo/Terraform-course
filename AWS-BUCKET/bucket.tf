93888f6b2f107e9771425fcbc31cede37ff9438e
Joao <joao.c@outlook.com>
2024-08-09 16:27:35 -0300

    criar bucket aws

---
+++ AWS-BUCKET/bucket.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "nome-do-bucket" {
+    bucket = "colombo-bucket"  # nome deve ser unico globalmente
+}
---
+++ AWS-BUCKET/main.tf
@@ -0,0 +1,22 @@
+
+terraform {
+    required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "sa-east-1"  #colocar sa-east-1 para a região de São Paulo
+    
+    default_tags {
+      tags = {
+        owner = "colombo"
+        environment = "dev"
+        managed_by = "terraform"
+      }
+    }
+}
